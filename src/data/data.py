7af1108774b5764b8de2643ac5f2d24ead9b2784
Estefany <mechaparro81@ucatolica.edu.co>
2025-03-05 22:39:12 -0500

    class Data:

--- src/data/data.py
+++ src/data/data.py
@@ -3,7 +3,7 @@ class Data:
     Clase con métodos para operaciones y manipulaciones de estructuras de datos.
     Incluye implementaciones y algoritmos para arreglos, listas y otras estructuras.
     """
-
+    pass 
     def invertir_lista(self, lista):
         """
         Invierte el orden de los elementos en una lista sin usar reversed() o lista[::-1].
