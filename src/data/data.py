c6a87e581e54978d77318771178c608da1760e40
Estefany <mechaparro81@ucatolica.edu.co>
2025-03-08 19:08:25 -0500

    def es_subconjunto(self, conjunto1, conjunto2):

--- src/data/data.py
+++ src/data/data.py
@@ -66,16 +66,12 @@ class Data:
         pass
     
     def es_subconjunto(self, conjunto1, conjunto2):
-        """
-        Verifica si conjunto1 es subconjunto de conjunto2 sin usar set.
+        """Verifica si conjunto1 es subconjunto de conjunto2."""
+        for elemento in conjunto1:
+            if elemento not in conjunto2:
+                return False
+        return True
         
-        Args:
-            conjunto1 (list): Posible subconjunto
-            conjunto2 (list): Conjunto principal
-            
-        Returns:
-            bool: True si conjunto1 es subconjunto de conjunto2, False en caso contrario
-        """
         pass
     
     def implementar_pila(self):
