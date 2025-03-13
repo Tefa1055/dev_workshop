2ece0cfe0fde11415984efec6392f87738198949
Estefany <mechaparro81@ucatolica.edu.co>
2025-03-13 15:51:20 -0500

    def area_superficie_cubo(self, lado):

--- src/geometric/geometric.py
+++ src/geometric/geometric.py
@@ -210,6 +210,7 @@ class Geometria:
         Returns:
             float: Área de la superficie del cubo
         """
+        return 6 * (lado ** 2)
         pass
     
     def volumen_esfera(self, radio):
