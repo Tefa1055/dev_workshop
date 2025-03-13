68323f0b967747aebf4ef8be90bd94134937bbe8
Estefany <mechaparro81@ucatolica.edu.co>
2025-03-13 16:11:17 -0500

    def area_poligono_regular(self, num_lados, lado, apotema):

--- src/geometric/geometric.py
+++ src/geometric/geometric.py
@@ -350,6 +350,7 @@ class Geometria:
         Returns:
             float: Área del polígono regular
         """
+        return (num_lados * lado * apotema) / 2
         pass
     
     def perimetro_poligono_regular(self, num_lados, lado):
