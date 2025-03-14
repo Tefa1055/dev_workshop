55b6528419565b595fa401358635771e3a268aa9
Estefany <mechaparro81@ucatolica.edu.co>
2025-03-13 23:07:49 -0500

    def es_primo(self, n):

--- src/magic/magic.py
+++ src/magic/magic.py
@@ -50,6 +50,12 @@ class Magic:
         Returns:
             bool: True si n es primo, False en caso contrario
         """
+        if n < 2:
+            return False
+        for i in range(2, int(n ** 0.5) + 1):
+            if n % i == 0:
+                return False
+        return True
         pass
     
     def generar_primos(self, n):
